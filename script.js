c99c79b9751931e9833f62854c1b5a658fbfd230
Dariusz Podczasik <boostertech@mail.com>
2023-05-09 21:10:41 +0100

    init() function added & some button functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+{
+    const tasks = [];
+
+    const init = () => {
+        const form = document.querySelector(".js-addTaskForm");
+
+        form.addEventListener("submit", event => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-addTaskForm__input").value.trim();
+
+            if (newTaskContent === "")
+                return;
+            console.log(newTaskContent);
+        })
+    }
+
+    init();
+}
